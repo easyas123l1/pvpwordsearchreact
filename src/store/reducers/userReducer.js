f1cf00bfb9ec93e1cca58d3bacf241e7d3f84b94
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-08-28 08:17:09 -0500

    able to work offline

--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -1,66 +1,66 @@
 import {
-  LOGIN_SUCCESS,
-  BEGIN_GET_USER_INFO,
-  GET_USER_INFO_SUCCESS,
-  GET_USER_INFO_FAILURE,
+    LOGIN_SUCCESS,
+    BEGIN_GET_USER_INFO,
+    GET_USER_INFO_SUCCESS,
+    GET_USER_INFO_FAILURE,
 } from "../actions/userAction";
 
 const initState = {
-  user: {
-    email: "",
-    id: null,
-    imageUrl: "",
-  },
-  isAuthenticating: false,
-  loggedIn: false,
-  authenticationError: "",
+    user: {
+        email: "fake", //this needs to be changed to ''
+        id: null,
+        imageUrl: "",
+    },
+    isAuthenticating: false,
+    loggedIn: true, //this needs to be changed to false
+    authenticationError: "",
 };
 
 export const userReducer = (state = initState, action) => {
-  switch (action.type) {
-    case LOGIN_SUCCESS:
-      return {
-        ...state,
-        user: {
-          email: action.payload.email,
-          id: action.payload.id,
-          imageUrl: action.payload.imageUrl,
-        },
-        isAuthenticating: false,
-        loggedIn: action.payload.isSignedIn,
-        authenticationError: "",
-      };
+    switch (action.type) {
+        case LOGIN_SUCCESS:
+            return {
+                ...state,
+                user: {
+                    email: action.payload.email,
+                    id: action.payload.id,
+                    imageUrl: action.payload.imageUrl,
+                },
+                isAuthenticating: false,
+                loggedIn: action.payload.isSignedIn,
+                authenticationError: "",
+            };
 
-    case BEGIN_GET_USER_INFO:
-      return {
-        ...state,
-        isAuthenticating: true,
-        authenticationError: "",
-      };
+        case BEGIN_GET_USER_INFO:
+            return {
+                ...state,
+                isAuthenticating: true,
+                authenticationError: "",
+            };
 
-    case GET_USER_INFO_SUCCESS:
-      return {
-        ...state,
-        isAuthenticating: false,
-        loggedIn: true,
-        authenticationError: "",
-        user: {
-          ...state.user,
-          name: action.payload.username,
-          id: action.payload.id,
-        },
-      };
+        case GET_USER_INFO_SUCCESS:
+            return {
+                ...state,
+                isAuthenticating: false,
+                loggedIn: true,
+                authenticationError: "",
+                user: {
+                    ...state.user,
+                    name: action.payload.username,
+                    id: action.payload.id,
+                },
+            };
 
-    case GET_USER_INFO_FAILURE:
-      return {
-        ...state,
-        isAuthenticating: false,
-        loggedIn: false,
-        authenticationError: action.payload,
-      };
-    default:
-      return {
-        ...state,
-      };
-  }
+        case GET_USER_INFO_FAILURE:
+            return {
+                ...state,
+                isAuthenticating: false,
+                loggedIn: false,
+                authenticationError: action.payload,
+            };
+        default:
+            return {
+                ...state,
+            };
+    }
 };
