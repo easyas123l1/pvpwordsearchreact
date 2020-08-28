8a21eae81f54ed78d904c5525d6d5aeafb7a8a5b
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-08-28 05:08:11 -0500

    add tons of changes for socket.io

---
+++ src/components/SocketGame/GameRoomLobby.js
@@ -0,0 +1,14 @@
+import React from "react";
+import puzzle from "../../styles/puzzle.module.scss";
+
+const GameRoomLobby = () => {
+    return (
+        <div className={puzzle.spacer}>
+            <div className={puzzle.background}>
+                <div>inside some lobby WOW!</div>
+            </div>
+        </div>
+    );
+};
+
+export default GameRoomLobby;
