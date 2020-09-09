0952c6eb7e37f7344f63a76e2eab4ff4393a00da
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-09-09 10:19:59 -0500

    text to hold page for now

--- src/components/SocketGame/GameRoomLobby.js
+++ src/components/SocketGame/GameRoomLobby.js
@@ -1,11 +1,22 @@
 import React from "react";
 import puzzle from "../../styles/puzzle.module.scss";
 
-const GameRoomLobby = () => {
+const GameRoomLobby = ({ room, leaveRoom }) => {
     return (
         <div className={puzzle.spacer}>
             <div className={puzzle.background}>
-                <div>inside some lobby WOW!</div>
+                <div>inside some lobby game started! WOW!</div>
+                <p>players</p>
+                <p>roomid {room.id}</p>
+                <p>room name {room.name}</p>
+                <p>room state {room.state}</p>
+                <p>room hostId {room.hostId}</p>
+                <p>PUZZLE</p>
+                <p>puzzle size {room.puzzle.size}</p>
+                <p>puzzle number of words {room.puzzle.numberOfWords}</p>
+                <p>puzzle timer {room.puzzle.timer}</p>
+                <p>puzzle code {room.puzzle.puzzle}</p>
+                <button onClick={leaveRoom}>Leave</button>
             </div>
         </div>
     );
