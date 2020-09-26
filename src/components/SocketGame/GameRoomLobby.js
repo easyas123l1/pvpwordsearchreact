1c2e3c85f60e23ac43b5fb5c4647a95297214945
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-09-26 10:16:55 -0500

    update comment

--- src/components/SocketGame/GameRoomLobby.js
+++ src/components/SocketGame/GameRoomLobby.js
@@ -1,12 +1,14 @@
 import React from "react";
 import puzzle from "../../styles/puzzle.module.scss";
-import PlayPuzzle from './PlayPuzzle';
+import PlayPuzzle from "./PlayPuzzle";
 
-const GameRoomLobby = ({ room, leaveRoom }) => {
+const GameRoomLobby = ({ room, leaveRoom, serverId }) => {
+    // mywords grabs a players words by filtering by id and destructuring first out of array.
+    const [myWords] = room.players.filter((player) => serverId === player.id);
     return (
         <div className={puzzle.spacer}>
             <div className={puzzle.background}>
-                <div>inside some lobby game started! WOW!</div>
+                {/* <div>inside some lobby game started! WOW!</div>
                 <p>players</p>
                 <p>roomid {room.id}</p>
                 <p>room name {room.name}</p>
@@ -16,8 +18,12 @@ const GameRoomLobby = ({ room, leaveRoom }) => {
                 <p>puzzle size {room.puzzle.size}</p>
                 <p>puzzle number of words {room.puzzle.numberOfWords}</p>
                 <p>puzzle timer {room.puzzle.timer}</p>
-                <p>puzzle code {room.puzzle.puzzle ? "true" : "false"}</p>
-                <PlayPuzzle words={room.puzzle.wordsDir} name={room.name} code={room.puzzle.puzzle} />
+                <p>puzzle code {room.puzzle.puzzle ? "true" : "false"}</p> */}
+                <PlayPuzzle
+                    words={myWords.wordsDir}
+                    name={room.name}
+                    code={room.puzzle.puzzle}
+                />
                 <button onClick={leaveRoom}>Leave</button>
             </div>
         </div>
