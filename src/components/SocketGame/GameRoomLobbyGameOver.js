612e8ee78a5649de6c831b5e150100cead0cb765
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-10-14 02:43:33 -0500

    new placeholder page

--- src/components/SocketGame/GameRoomLobbyGameOver.js
+++ src/components/SocketGame/GameRoomLobbyGameOver.js
@@ -2,11 +2,28 @@ import React from "react";
 import puzzle from "../../styles/puzzle.module.scss";
 
 export default function GameRoomLobbyGameOver({ room, leaveRoom, serverId }) {
-    return (
-        <div className={puzzle.spacer}>
-            <div className={puzzle.background}>
-                <div>inside some lobby where the game ended!</div>
+    // mywords grabs a players words by filtering by id and destructuring first out of array.
+    const [myWords] = room.players.filter((player) => serverId === player.id);
+    if (myWords) {
+        return (
+            <div className={puzzle.spacer}>
+                <div className={puzzle.background}>
+                    <p>inside some lobby where the game ended!</p>
+                    <button onClick={leaveRoom}>Leave</button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return (
+            <div className={puzzle.spacer}>
+                <div className={puzzle.background}>
+                    <p>
+                        server most likely ran into error/reset please refresh
+                        page.
+                    </p>
+                    <button onClick={leaveRoom}>Leave</button>
+                </div>
+            </div>
+        );
+    }
 }
