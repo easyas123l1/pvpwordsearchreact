3dccffb1712e6637128fa0b4be07987743954653
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-11-05 01:26:42 -0600

    add more information to Room cards

--- src/components/SocketGame/RoomListCard.js
+++ src/components/SocketGame/RoomListCard.js
@@ -1,9 +1,21 @@
 import React from "react";
+import puzzle from "../../styles/puzzle.module.scss";
 
+// time to make the rooms look better!
 export default function RoomListCard({ room, joinGame }) {
+    console.log(room);
+    const [host] = room.players.filter((player) => room.hostId === player.id);
     return (
-        <div>
-            <p>{room.name}</p>
+        <div className={puzzle.joinGameBox}>
+            <p>HOST: {host.email} </p>
+            <p>NAME: {room.name} </p>
+            <p>MAXIMUM WORD SIZE: {room.puzzle.maximumWordSize} </p>
+            <p>MINIMUM WORD SIZE: {room.puzzle.minimumWordSize} </p>
+            <p>NUMBER OF WORDS: {room.puzzle.numberOfWords} </p>
+            <p>
+                PUZZLE DIMENSIONS: {room.puzzle.size}x{room.puzzle.size}
+            </p>
+            <p>TIMER(seconds): {room.puzzle.timer}</p>
             <button id={room.id} onClick={joinGame}>
                 Join
             </button>
