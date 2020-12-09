6451fcada2f664ee1c33e04a187ef78e215efb7d
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-12-08 23:13:13 -0600

    add styling to profile page.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,12 @@
 import React from "react";
+import puzzle from "../../styles/puzzle.module.scss";
 
-export default function Profile() {
-    return <div>hi</div>;
+export default function Profile({ email, name }) {
+    return (
+        <div className={puzzle.spacer}>
+            <div className={puzzle.background}>
+                <p>hi {name}</p>
+            </div>
+        </div>
+    );
 }
