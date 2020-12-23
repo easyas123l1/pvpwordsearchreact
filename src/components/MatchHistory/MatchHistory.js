90c44f9a472136700a8610b7c8c24c052ae37546
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-12-23 13:59:45 -0600

    get users match history

--- src/components/MatchHistory/MatchHistory.js
+++ src/components/MatchHistory/MatchHistory.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import puzzle from "../../styles/puzzle.module.scss";
+import axios from "axios";
 
 export default function MatchHistory({ email }) {
+    useEffect(() => {
+        axios
+            .post(
+                "https://pvpwordsearc-master-fyw6qrqfuj.herokuapp.com/puzzle/user/history",
+                { email }
+            )
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, [email]);
     return (
         <div className={puzzle.spacer}>
             <div className={puzzle.background}>
