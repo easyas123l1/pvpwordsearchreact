0e1719a7aa581264b7b9789e16a1fbd291f86772
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-12-30 03:47:51 -0600

    show previous puzzles

--- src/components/MatchHistory/MatchHistory.js
+++ src/components/MatchHistory/MatchHistory.js
@@ -1,15 +1,29 @@
 import React, { useState, useEffect } from "react";
 import puzzle from "../../styles/puzzle.module.scss";
 import MatchCard from "./MatchCard";
+import PuzzleView from "./PuzzleView";
 import axios from "axios";
 
 export default function MatchHistory({ email }) {
     const [history, setHistory] = useState([]);
+    const [puzzleVisibile, setPuzzleVisibile] = useState(false);
+    const [puzzleView, setPuzzleView] = useState([]);
+
+    const viewPuzzle = (puzzle) => {
+        setPuzzleVisibile(true);
+        setPuzzleView(puzzle);
+    };
+
+    const leavePuzzle = () => {
+        setPuzzleVisibile(false);
+        setPuzzleView([]);
+    };
 
     useEffect(() => {
         axios
             .post(
-                "https://pvpwordsearc-master-fyw6qrqfuj.herokuapp.com/puzzle/user/history",
+                // "https://pvpwordsearc-master-fyw6qrqfuj.herokuapp.com/puzzle/user/history",
+                "http://localhost:4999/puzzle/user/history",
                 { email }
             )
             .then((res) => {
@@ -20,13 +34,31 @@ export default function MatchHistory({ email }) {
                 console.log(err);
             });
     }, [email]);
+    if (puzzleVisibile) {
+        return (
+            <div className={puzzle.spacer}>
+                <div className={puzzle.background}>
+                    <PuzzleView
+                        puzzleView={puzzleView}
+                        leavePuzzle={leavePuzzle}
+                    />
+                </div>
+            </div>
+        );
+    }
     if (history.length > 0) {
         return (
             <div className={puzzle.spacer}>
                 <div className={puzzle.background}>
                     <p>hi {email}</p>
                     {history.map((puzzles) => {
-                        return <MatchCard key={puzzles.id} puzzle={puzzles} />;
+                        return (
+                            <MatchCard
+                                key={puzzles.id}
+                                puzzle={puzzles}
+                                viewPuzzle={viewPuzzle}
+                            />
+                        );
                     })}
                 </div>
             </div>
