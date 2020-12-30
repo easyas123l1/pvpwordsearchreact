9c4b668362470af8f3b9d72d521942a8c373396c
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-12-30 03:45:58 -0600

    map users words and add button

--- src/components/MatchHistory/MatchCard.js
+++ src/components/MatchHistory/MatchCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function MatchCard({ puzzle }) {
+export default function MatchCard({ puzzle, viewPuzzle }) {
     return (
         <div>
             <p>id: {puzzle.id}</p>
@@ -10,11 +10,20 @@ export default function MatchCard({ puzzle }) {
             <p>name: {puzzle.name}</p>
             <p>number of words: {puzzle.number_of_words}</p>
             <p>rating: coming soon! {puzzle.rating}</p>
-            <p>size: {puzzle.size}</p>
+            <p>
+                size: {puzzle.size}x{puzzle.size}
+            </p>
             <p>time: {puzzle.time}</p>
             <p>users:</p>
+            {puzzle.users.map((user) => {
+                return <p key={user.games_users_id}>{user.name}</p>;
+            })}
             <p>words:</p>
+            {puzzle.words.map((word) => {
+                return <p key={word.id}>{word.word}</p>;
+            })}
             <p>puzzle: puzzle.code lol</p>
+            <button onClick={() => viewPuzzle(puzzle)}>Click</button>
         </div>
     );
 }
