739bf50973db5b1844f3912804b48d82976bd4ac
Andrew Wilson <andrew.wilson90@hotmail.com>
2021-03-30 20:09:42 -0500

    split create game and join game screens

--- src/components/SocketGame/MainScreenFindCreateGame.js
+++ src/components/SocketGame/MainScreenFindCreateGame.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import puzzle from "../../styles/puzzle.module.scss";
 import RoomListCard from "./RoomListCard";
 
@@ -9,70 +9,84 @@ const MainScreenFindCreateGame = ({
     rooms,
     joinGame,
 }) => {
-    return (
-        <div className={puzzle.spacer}>
-            <div className={puzzle.background}>
-                <div>
-                    <p>Create a lobby...</p>
-                    <p>Name</p>
-                    <input
-                        type="text"
-                        name="name"
-                        onChange={changeGameObj}
-                        placeholder="Title the game room!"
-                        value={createGameObj.name}
-                        className={puzzle.inputField}
-                    />
-                    <p>Size</p>
-                    <input
-                        type="number"
-                        name="size"
-                        onChange={changeGameObj}
-                        value={createGameObj.size}
-                        className={puzzle.inputField}
-                    />
-                    <p>Number of Words</p>
-                    <input
-                        type="number"
-                        name="numberOfWords"
-                        onChange={changeGameObj}
-                        value={createGameObj.numberOfWords}
-                        className={puzzle.inputField}
-                    />
-                    <p>Time in seconds</p>
-                    <input
-                        type="number"
-                        name="timer"
-                        onChange={changeGameObj}
-                        value={createGameObj.timer}
-                        className={puzzle.inputField}
-                    />
-                    <p>Minimum word length</p>
-                    <input
-                        type="number"
-                        name="minimumWordSize"
-                        onChange={changeGameObj}
-                        value={createGameObj.minimumWordSize}
-                        className={puzzle.inputField}
-                    />
-                    <p>Maximum word length</p>
-                    <input
-                        type="number"
-                        name="maximumWordSize"
-                        onChange={changeGameObj}
-                        value={createGameObj.maximumWordSize}
-                        className={puzzle.inputField}
-                    />
-                    <p>------------</p>
-                    <button
-                        onClick={createGame}
-                        className={puzzle.createGameButton}
-                    >
-                        Create Lobby
-                    </button>
+    const [creatingGame, setCreatingGame] = useState(false);
+    if (creatingGame) {
+        return (
+            <div className={puzzle.spacer}>
+                <div className={puzzle.background}>
+                    <div>
+                        <button onClick={() => setCreatingGame(false)}>
+                            Go Back to join a lobby
+                        </button>
+                        <p>Create a lobby...</p>
+                        <p>Name</p>
+                        <input
+                            type="text"
+                            name="name"
+                            onChange={changeGameObj}
+                            placeholder="Title the game room!"
+                            value={createGameObj.name}
+                            className={puzzle.inputField}
+                        />
+                        <p>Size</p>
+                        <input
+                            type="number"
+                            name="size"
+                            onChange={changeGameObj}
+                            value={createGameObj.size}
+                            className={puzzle.inputField}
+                        />
+                        <p>Number of Words</p>
+                        <input
+                            type="number"
+                            name="numberOfWords"
+                            onChange={changeGameObj}
+                            value={createGameObj.numberOfWords}
+                            className={puzzle.inputField}
+                        />
+                        <p>Time in seconds</p>
+                        <input
+                            type="number"
+                            name="timer"
+                            onChange={changeGameObj}
+                            value={createGameObj.timer}
+                            className={puzzle.inputField}
+                        />
+                        <p>Minimum word length</p>
+                        <input
+                            type="number"
+                            name="minimumWordSize"
+                            onChange={changeGameObj}
+                            value={createGameObj.minimumWordSize}
+                            className={puzzle.inputField}
+                        />
+                        <p>Maximum word length</p>
+                        <input
+                            type="number"
+                            name="maximumWordSize"
+                            onChange={changeGameObj}
+                            value={createGameObj.maximumWordSize}
+                            className={puzzle.inputField}
+                        />
+                        <p>------------</p>
+                        <button
+                            onClick={createGame}
+                            className={puzzle.createGameButton}
+                        >
+                            Create Lobby
+                        </button>
+                    </div>
                 </div>
-                <div>
-                    <p>join a lobby</p>
+            </div>
+        );
+    } else {
+        return (
+            <div className={puzzle.spacer}>
+                <div className={puzzle.background}>
+                    <button onClick={() => setCreatingGame(true)}>
+                        Create a lobby
+                    </button>
+                    <p>Join a lobby</p>
                     <div>
                         {rooms.map((room) => {
                             return (
@@ -86,8 +100,8 @@ const MainScreenFindCreateGame = ({
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
 
 export default MainScreenFindCreateGame;
