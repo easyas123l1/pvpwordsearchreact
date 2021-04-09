d778e9f60e6260cdd11fe6e5597fc9d75e2b2652
Andrew Wilson <andrew.wilson90@hotmail.com>
2021-04-08 21:04:54 -0500

    add styling and commented out old html

--- src/components/MatchHistory/MatchCard.js
+++ src/components/MatchHistory/MatchCard.js
@@ -1,32 +1,32 @@
 import React from "react";
+import puzzles from "../../styles/puzzle.module.scss";
 
 export default function MatchCard({ puzzle, viewPuzzle }) {
     const handleClick = () => {
         viewPuzzle(puzzle);
     };
     return (
-        <div>
-            <p>id: {puzzle.id}</p>
+        <div className={puzzles.matchHistory}>
+            <p>Name: {puzzle.name}</p>
             <p>description: {puzzle.description}</p>
             <p>maximum word length: {puzzle.maximum_word_length}</p>
             <p>minimum word length: {puzzle.minimum_word_length}</p>
-            <p>name: {puzzle.name}</p>
-            <p>number of words: {puzzle.number_of_words}</p>
-            <p>rating: coming soon! {puzzle.rating}</p>
+            <p>Mumber of words: {puzzle.number_of_words}</p>
+            <p>Rating: coming soon! {puzzle.rating}</p>
             <p>
-                size: {puzzle.size}x{puzzle.size}
+                Size: {puzzle.size}x{puzzle.size}
             </p>
-            <p>time: {puzzle.time}</p>
-            <p>users:</p>
-            {puzzle.users.map((user) => {
+            <p>Time: {puzzle.time}</p>
+            <p>Users: {puzzle.users.length}</p>
+            {/* {puzzle.users.map((user) => {
                 return <p key={user.games_users_id}>{user.name}</p>;
-            })}
-            <p>words:</p>
+            })} */}
+            {/* <p>Words:</p>
             {puzzle.words.map((word) => {
                 return <p key={word.id}>{word.word}</p>;
-            })}
-            <p>puzzle: puzzle.code lol</p>
-            <button onClick={handleClick}>Click</button>
+            })} */}
+            {/* <p>puzzle: puzzle.code lol</p> */}
+            <button onClick={handleClick}>View Match</button>
         </div>
     );
 }
